de8854c4be43b21f573ada1c71e7da947e1ae183
elberthmoraes <elberth.moraes@gmail.com>
2018-09-03 08:47:42 -0300

    Commit - comentários:

--- AppReview/src/testes/TestaCliente.java
+++ AppReview/src/testes/TestaCliente.java
@@ -9,11 +9,13 @@ public class TestaCliente {
 		Cliente c1 = new Cliente("joao", "12345678901", "centro", "rio de janeiro", "RJ");
 		c1.exibir();
 		
+		//Segunda instância
 		Cliente c2 = new Cliente("icarai", "niteroi", "RJ");
 		c2.nome = "maria";
 		c2.cpf = "98765432101";
 		c2.exibir();
 		
+		//Terceira instância
 		Cliente c3 = new Cliente();
 		c3.exibir();
 	}
